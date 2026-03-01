869cda0e680a20319e54de8c5521f072a9d8a874
gustavoLim4 <gustavolll15567@gmail.com>
2026-03-01 18:58:44 -0300

    Home ja pronta

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Home} from "./pages/Home/Home";
 import {Layout} from "./components/layout/Layout";
 import { Sobre } from "./pages/Sobre/Sobre";
